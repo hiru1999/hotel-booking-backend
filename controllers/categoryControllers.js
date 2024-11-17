b6d869d560898d5853f5dfded202008f5d8d59a4
hiru1999 <hiruvisenadeera@gmail.com>
2024-11-17 13:53:42 +0530

    Create postCategory function

---
+++ controllers/categoryControllers.js
@@ -0,0 +1,33 @@
+import Category from "../models/category";
+
+//post - create
+export function postCategory(req,res){
+    const user = req.user
+    if(user == null){
+        res.status(403).json({
+            message : "Please login to create a category"
+        })
+        return 
+    }
+    if(user?.type != "admin"){
+        res.status(403).json({
+            message : "You are not authorized to create a category"
+        })
+        return
+    }
+    const category = req.body.item
+    const newCategory = new Category(category)
+    newCategory.save().then(
+        ()=>{
+            res.json({
+                message : "Category created successfully"
+            })
+        }
+    ).catch(
+        ()=>{
+            res.status(500).json({
+                message : "Category item creation failed"
+            })
+        }
+    )
+}
