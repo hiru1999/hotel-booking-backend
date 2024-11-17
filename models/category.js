91c09a4a8b6778cd7770edfffa00551daf1b45a7
hiru1999 <hiruvisenadeera@gmail.com>
2024-11-17 13:47:00 +0530

    Create category model

---
+++ models/category.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const categorySchema = mongoose.Schema(
+    {
+        name : {
+            type : String,
+            required : true
+        },
+        price : {
+            type : Number,
+            required : true
+        },
+        description : {
+            type : String,
+            required : true
+        }
+    }
+    
+)
+
+const Category = mongoose.model("categories",categorySchema)
+export default Category;
