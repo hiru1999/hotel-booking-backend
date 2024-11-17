3d1959d3fc8bc3e068575674f8b433ad6fae3de9
hiru1999 <hiruvisenadeera@gmail.com>
2024-11-17 13:55:11 +0530

    Create getCategory function

--- controllers/categoryControllers.js
+++ controllers/categoryControllers.js
@@ -31,3 +31,14 @@ export function postCategory(req,res){
         }
     )
 }
+
+//get
+export function getCategory(req,res){
+    Category.find().then(
+        (list)=>{
+            res.json({
+                list : list
+            })
+        }
+    )
+}
