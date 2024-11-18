6237a9ae3762ed8c275a3e734e468a0607d53b65
hiru1999 <hiruvisenadeera@gmail.com>
2024-11-18 17:38:01 +0530

    Create createBooking function

--- controllers/bookingControllers.js
+++ controllers/bookingControllers.js
@@ -1,6 +1,15 @@
 import Booking from "../models/booking.js";
+import { isCustomerValid } from "./userControllers.js";
 
 export function createBooking(req,res){
+
+    if(!isCustomerValid(req)){
+        res.status(401).json({
+            message : "Unauthorized"
+        })
+        return
+    }  
+
     const startingId = 1200;
 
     Booking.countDocuments({}).then(
@@ -8,35 +17,38 @@ export function createBooking(req,res){
             console.log(count);
             const newId = startingId + count + 1;
             // const newId = "INV" + startingId + count + 1;
+            const newBooking = new Booking({
+                bookingId : newId,
+                roomId : req.body.roomId,
+                email : req.user.email,
+                start : req.body.start,
+                end : req.body.end,
+            })
+            newBooking.save().then(
+                (result)=>{
+        
+                    res.json({
+                        message : "Booking created successfully",
+                        result : result
+                    })
+                }
+            ).catch(
+                (err)=>{
+                    res.status(500).json({
+                        message : "Booking creation failed",
+                        error : err
+                    })
+                }
+            )
+        }
+    ).catch(
+        (err)=>{
+            res.status(500).json({
+                message : "Booking creation failed",
+                error : err
+            })
         }
     )
 }
 
 
-// export function createBooking(req,res){
-//     if(!isAdminValid(req)){
-//         res.status(401).json({
-//             message : "Unauthorized"
-//         })
-//     return
-//     }  
-    
-//     const room = req.body
-//     const newRoom = new Room(room)
-//     newRoom.save().then(
-//         (result)=>{
-
-//             res.json({
-//                 message : "Room created successfully",
-//                 result : result
-//             })
-//         }
-//     ).catch(
-//         (err)=>{
-//             res.status(500).json({
-//                 message : "Room creation failed",
-//                 error : err
-//             })
-//         }
-//     )
-// }
